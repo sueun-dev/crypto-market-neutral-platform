50c7803395113e11544becd758f361728b405aeb
sueun-dev <57546981+sueun-dev@users.noreply.github.com>
2026-03-11 10:47:58 -0400

    Remove absorbed source archives

--- legacy-imports/hedge-pilot/src/exchanges/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-거래소 모듈
-"""
-from .upbit import UpbitExchange
-from .bithumb import BithumbExchange
-from .gateio import GateIOExchange
-
-__all__ = ['UpbitExchange', 'BithumbExchange', 'GateIOExchange']
--- legacy-imports/hedge-pilot/src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""유틸리티 모듈"""
-from .logger import setup_logging
-
-__all__ = ['setup_logging']
